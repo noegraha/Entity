548fce9b59f8bdd980df1aea2a939496cdb53434
nugraha.triatmaja@gmail.com <nugraha.triatmaja@gmail.com>
2019-08-06 09:18:30 +0700

    Add project files.

---
+++ Models/SampleContext.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+namespace Entity.Models
+{
+    public partial class SampleContext : DbContext
+    {
+        public SampleContext()
+        {
+        }
+
+        public SampleContext(DbContextOptions<SampleContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Teacher> Teacher { get; set; }
+
+        // Unable to generate entity type for table 'dbo.User'. Please see the warning messages.
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. See http://go.microsoft.com/fwlink/?LinkId=723263 for guidance on storing connection strings.
+                optionsBuilder.UseSqlServer("Server=DESKTOP-8KP6712;Database=Sample;Trusted_Connection=True;");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("ProductVersion", "2.2.6-servicing-10079");
+
+            modelBuilder.Entity<Teacher>(entity =>
+            {
+                entity.Property(e => e.AddedOn)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Address)
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Name)
+                    .IsRequired()
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.NoHp)
+                    .HasColumnName("No HP")
+                    .HasMaxLength(50)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Salary).HasColumnType("money");
+
+                entity.Property(e => e.Skills)
+                    .IsRequired()
+                    .HasMaxLength(250)
+                    .IsUnicode(false);
+
+                entity.Property(e => e.Tanggal).HasColumnType("datetime");
+            });
+        }
+    }
+}
---
+++ Models/Teacher.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Entity.Models
+{
+    public partial class Teacher
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Skills { get; set; }
+        public int TotalStudents { get; set; }
+        public decimal Salary { get; set; }
+        public string AddedOn { get; set; }
+        public string Address { get; set; }
+        public string NoHp { get; set; }
+        public DateTime? Tanggal { get; set; }
+    }
+}
